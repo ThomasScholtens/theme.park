08a6af40498df2f8bc2ac2a1579352b1e02c324b
Thomanius Maximus Maeridius III <thomas.scholtens.scholtens@student.nhlstenden.com>
2025-04-15 23:38:06 +0200

    Update please_stand_by.js

--- css/addons/unraid/login-page/fallout/js/please_stand_by.js
+++ css/addons/unraid/login-page/fallout/js/please_stand_by.js
@@ -1,7 +1,10 @@
-// Adds a video background
-// Source: https://www.youtube.com/watch?v=ehvduomE0AU
+let videoHTML = `
+<div class="background-wrap">
+    <video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
+        <source src="https://your-correct-link-to-the-video.mp4" type="video/mp4">
+        Video not supported
+    </video>
+</div>
+`;
 
-let video = `
-<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/gilbn.github.io\/theme.park\/css\/addons\/unraid\/login-page\/fallout\/video\/please_stand_by.mp4" type="video\/mp4">Video not supported</video></div>
-`
-document.body.innerHTML += video
+document.body.innerHTML += videoHTML;
